9f9996d94fa0a36564e9294597de7310ed5d7a51
larr1 <kurochkin.vasiliy90@gmail.com>
2023-12-12 17:48:27 +0300

    randomChar + charList are done

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,92 @@
+import { Component } from 'react';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+
+class CharList extends Component {
+
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.marvelService.getAllChars()
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
+
+    onCharListLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updateCharList = () => {
+        this.marvelService
+                .getAllChars()
+                .then(this.onCharListLoaded)
+                .catch(this.onError)
+    }
+
+    renderItems(arr) {
+        const listItems = arr.map(item => {
+            const {name,thumbnail} = item;
+            let imgStyle = {objectFit: 'cover'}
+            if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {objectFit: 'unset'}
+            }
+            return (
+                <li 
+                    className="char__item char__item_selected"
+                    key={item.id}>
+                        <img src={thumbnail} alt="abyss" style={imgStyle}/>
+                        <div className="char__name">{name}</div>
+                </li>
+            );
+        });
+
+        return (
+            <ul className="char__grid">
+                {listItems}
+            </ul>
+        )
+    }
+
+    render() {
+        const {charList, error, loading} = this.state;
+
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default CharList;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+    _apiKey = 'apikey=cd24f7ffaf509bbb5cce629a883dd0f8'
+
+    getResourse = async (url) => {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    getAllChars = async () => {
+        const res= await this.getResourse(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getChar = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'We know nothing about this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
